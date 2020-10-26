218f41c5c0dc10758b10ad64a410bf44bc5e801e
João <joao.cortes@aluno.faculdadeimpacta.com.br>
2020-10-26 16:14:45 -0300

    ac4

--- app.py
+++ app.py
@@ -1,27 +1,27 @@
-importar  os
-from  flask  import  Flask , jsonify , request
+import os
+from flask import Flask, jsonify, request
 
-app  =  Flask ( __name__ )
+app = Flask(__name__)
 
-@ app . rota ( '/' )
-def  main ():
-  números  = []
+@app.route('/')
+def main():
+  numbers = []
 
-  nterms  =  50
+  nterms = 50
 
-  n1 , n2  =  0 , 1
-  contagem  =  0
+  n1, n2 = 0, 1
+  count = 0
 
-  enquanto  conta  <  ntermos :
-    números . anexar ( f " { n1 } " )
-    enésimo  =  n1  +  n2
+  while count < nterms:
+    numbers.append(f"{n1}")
+    nth = n1 + n2
 
-    n1  =  n2
-    n2  =  enésimo
-    contagem  + =  1
+    n1 = n2
+    n2 = nth
+    count += 1
 
-  retornar  "<br>" . juntar ( números )
+  return "<br>".join(numbers)
 
-if  __name__  ==  '__main__' :
-  porta  =  int ( os . amb . get ( "PORT" , 5000 ))
-  app . executar ( host = '0.0.0.0' , porta = porta )
+if __name__ == '__main__':
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host='0.0.0.0', port=port)
